a5a3b5d26e0d4c92a606cb6963fbd73923684251
Pardo <57932880+Snxi0@users.noreply.github.com>
2025-05-20 12:41:15 -0500

    Add files via upload

---
+++ Proyecto/confi-usuario/configuracion.php
@@ -0,0 +1,74 @@
+<?php include("../crud/db.php") ?><!--apenas inicie la apliacion empice a conectar la base de datos-->
+<?php	include("../includes/header.php")?>
+<?php	include("../includes/session_cd.php")?>
+
+
+
+<div class="container-fluid ">
+    <div class="row flex-nowrap w-100">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 custom-aside">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white">
+        
+                <ul class="nav nav-pills flex-column  align-items-center align-items-sm-start custom-ul" id="menu">
+                    <li class="nav-item">
+                        <a href="../crud/Crud.php" class="nav-link align-middle px-0 custom-icon <?= esActivo('inicio') ?>">
+                            <i class="fs-4 bi-house"> Inicio</i> 
+                            <span class="ms-1 d-none d-sm-inline text-white "></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../registro/table.php"  class="nav-link px-0 align-middle custom-icon <?= esActivo('platos') ?>">
+                            <i class="icon fs-4 bi-fork-knife veo"> Platos</i> <span class="ms-1 d-none d-sm-inline text-white"></span> </a>
+                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu"> 
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="ingredientes.php" class="nav-link px-0 align-middle custom-icon <?= esActivo('ingredientes') ?>">
+                            <i class="fs-4 bi bi-basket"> Ingredientes</i> <span class="ms-1 d-none d-sm-inline text-white"> </span></a>
+                    </li>
+                    <li>
+                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle custom-icon">
+                            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline text-white">-</span></a>
+                    </li>
+                    <li>
+                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle custom-icon">
+                            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline text-white">-</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle custom-icon <?= esActivo('usuarios') ?>">
+                            <i class="fs-4 bi-people"> Usuarios</i> <span class="ms-1 d-none d-sm-inline text-white"></span> </a>
+                    </li>
+                </ul>
+                <hr>
+                <div class="dropdown pb-4 ">
+                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle " id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                        <span class="d-none d-sm-inline mx-1">
+                        <?php echo $nombre . " " . $apellido; ?>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu text-small shadow custom-drop">
+                        <li><a class="dropdown-item custom-a" href="../confi-usuario/perfil.php"><i class="bi bi-person"></i>
+                        Profile</a></li>
+                        <li><a class="dropdown-item custom-a" href="../confi-usuario/configuracion.php"><i class="bi bi-gear"></i>
+                        Settings</a></li>
+                        <li>
+                            <hr class="dropdown-divider custom-hr">
+                        </li>
+                        <li><a class="dropdown-item custom-li" href="../logIn/cerrarsesion.php"><i class="bi bi-box-arrow-right me-2"></i>Cerrar Sesion</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col py-3 custom-bd">
+            <h1>configuracion</h1>
+
+            <div class="container p-4 ">
+            </div>
+
+
+
+
+
+
+<?php	include("../includes/footer.php")?>
---
+++ Proyecto/logIn/validar.php
@@ -0,0 +1,18 @@
+<?php
+include('../crud/db.php');
+
+$cedula = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
+
+$consulta = "SELECT * FROM usuarios WHERE cedula ='$cedula' and contraseña ='$contraseña'";
+$resultado = mysqli_query($conn, $consulta);
+
+if (mysqli_num_rows($resultado) > 0) { //Cuenta cuántos resultados devolvió la base de datos después de ejecutar la consulta. Si es mayor que 0, significa que el usuario y la contraseña están correctos.
+    $_SESSION['cedula'] = $cedula;
+    header("Location: ../crud/crud.php");
+    exit();
+} else {
+    $error = "⚠️ Usuario o contraseña incorrectos";
+}
+mysqli_free_result($resultado);
+?>
